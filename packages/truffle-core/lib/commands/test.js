50db81a3972a1e482d5aef1f847c4c8a00fbd9a4
be1box <mi.belbox@gmail.com>
2018-10-16 22:22:01 +0300

    Removed creation of temporary directory, disabled reset of migrations

--- packages/truffle-core/lib/commands/test.js
+++ packages/truffle-core/lib/commands/test.js
@@ -77,72 +77,64 @@ var command = {
         return file.match(config.test_file_extension_regexp) != null;
       });
 
-      temp.mkdir('test-', function(err, temporaryDirectory) {
+      function callback() {
+        var args = arguments;
+        done.apply(null, args);
+        if (ipcDisconnect) {
+          ipcDisconnect();
+        }
+      };
+
+      function run() {
+        // Set a new artifactor; don't rely on the one created by Environments.
+        // TODO: Make the test artifactor configurable.
+        config.artifactor = new Artifactor(config.contracts_build_directory);
+
+        Test.run(config.with({
+          test_files: files,
+          contracts_build_directory: temporaryDirectory
+        }), callback);
+      };
+
+      var environmentCallback = function(err) {
         if (err) return done(err);
+        // Copy all the built files over to a temporary directory, because we
+        // don't want to save any tests artifacts. Only do this if the build directory
+        // exists.
+        fs.stat(config.contracts_build_directory, function(err, stat) {
+          if (err) return run();
 
-        function cleanup() {
-          var args = arguments;
-          // Ensure directory cleanup.
-          temp.cleanup(function(err) {
-            // Ignore cleanup errors.
-            done.apply(null, args);
-            if (ipcDisconnect) {
-              ipcDisconnect();
-            }
+          copy(config.contracts_build_directory, temporaryDirectory, function(err) {
+            if (err) return done(err);
+
+            config.logger.log("Using network '" + config.network + "'." + OS.EOL);
+
+            run();
           });
+        });
+      }
+
+      if (config.networks[config.network]) {
+        Environment.detect(config, environmentCallback);
+      } else {
+        var ipcOptions = {
+          network: "test"
         };
 
-        function run() {
-          // Set a new artifactor; don't rely on the one created by Environments.
-          // TODO: Make the test artifactor configurable.
-          config.artifactor = new Artifactor(temporaryDirectory);
-
-          Test.run(config.with({
-            test_files: files,
-            contracts_build_directory: temporaryDirectory
-          }), cleanup);
+        var testrpcOptions = {
+          host: "127.0.0.1",
+          port: 7545,
+          network_id: 4447,
+          mnemonic: "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat",
+          gasLimit: config.gas,
+          noVMErrorsOnRPCResponse: true,
         };
 
-        var environmentCallback = function(err) {
-          if (err) return done(err);
-          // Copy all the built files over to a temporary directory, because we
-          // don't want to save any tests artifacts. Only do this if the build directory
-          // exists.
-          fs.stat(config.contracts_build_directory, function(err, stat) {
-            if (err) return run();
-
-            copy(config.contracts_build_directory, temporaryDirectory, function(err) {
-              if (err) return done(err);
-
-              config.logger.log("Using network '" + config.network + "'." + OS.EOL);
-
-              run();
-            });
-          });
-        }
-
-        if (config.networks[config.network]) {
-          Environment.detect(config, environmentCallback);
-        } else {
-          var ipcOptions = {
-            network: "test"
-          };
-
-          var testrpcOptions = {
-            host: "127.0.0.1",
-            port: 7545,
-            network_id: 4447,
-            mnemonic: "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat",
-            gasLimit: config.gas,
-            noVMErrorsOnRPCResponse: true,
-          };
-
-          Develop.connectOrStart(ipcOptions, testrpcOptions, function(started, disconnect) {
-            ipcDisconnect = disconnect;
-            Environment.develop(config, testrpcOptions, environmentCallback);
-          });
-        }
-      });
+        Develop.connectOrStart(ipcOptions, testrpcOptions, function(started, disconnect) {
+          ipcDisconnect = disconnect;
+          Environment.develop(config, testrpcOptions, environmentCallback);
+        });
+      }
     });
   }
 }
